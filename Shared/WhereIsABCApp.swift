d6e5b58b79584fb22f23de8528066198f1b916f0
Glenn Posadas <hello@glennvon.com>
2020-12-06 21:10:34 +0800

    add base project - multiplatform.

---
+++ Shared/WhereIsABCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereIsABCApp.swift
+//  Shared
+//
+//  Created by Glenn Posadas on 12/6/20.
+//
+
+import SwiftUI
+
+@main
+struct WhereIsABCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
