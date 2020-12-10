826958b3f1a9051d0a5924eb43e035a2f77b0cad
Glenn Posadas <hello@glennvon.com>
2020-12-11 04:01:39 +0800

    save progress

--- WhereIsABC/Views/SplashViewController.swift
+++ WhereIsABC/Views/SplashViewController.swift
@@ -5,11 +5,14 @@
 //  Created by Glenn Posadas on 12/10/20.
 //
 
+import MorphingLabel
 import UIKit
 
 class SplashViewController: UIViewController {
 
     // MARK: - Properties
+    
+    @IBOutlet weak var label_Title1: LTMorphingLabel!
     
     // MARK: - Overrides
     // MARK: Functions
