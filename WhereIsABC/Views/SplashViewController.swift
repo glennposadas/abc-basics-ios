b2e6645daf4e177604de929a71d8f5662cc9d2f8
Glenn Posadas <hello@glennvon.com>
2020-12-10 21:10:56 +0800

    start splash.

---
+++ WhereIsABC/Views/SplashViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SplashViewController.swift
+//  WhereIsABC
+//
+//  Created by Glenn Posadas on 12/10/20.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Overrides
+    // MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
