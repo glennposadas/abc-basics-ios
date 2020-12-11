b48a59d48aac833c61c15f6d08d71a8925d639e5
Glenn Posadas <hello@glennvon.com>
2020-12-11 23:51:46 +0800

    add assets/

---
+++ WhereIsABC/Views/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  WhereIsABC
+//
+//  Created by Glenn Posadas on 12/11/20.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Overrides
+    // MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
