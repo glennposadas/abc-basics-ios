10b1dd9363d08cf62d5990839ce5724fe0fe4872
Glenn Posadas <hello@glennvon.com>
2020-12-11 14:20:32 +0800

    add splash animation.

--- WhereIsABC/Views/SplashViewController.swift
+++ WhereIsABC/Views/SplashViewController.swift
@@ -8,11 +8,12 @@
 import MorphingLabel
 import UIKit
 
-class SplashViewController: UIViewController {
+class SplashViewController: BaseViewController {
 
     // MARK: - Properties
     
     @IBOutlet weak var label_Title1: LTMorphingLabel!
+    @IBOutlet weak var label_Title2: LTMorphingLabel!
     
     // MARK: - Overrides
     // MARK: Functions
@@ -20,5 +21,35 @@ class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupLabels()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animateLabels(withDelay: 1)
+    }
+    
+    private func setupLabels() {
+        let title1FontScale: CGFloat = 0.2
+        let title2FontScale: CGFloat = 0.4
+        
+        let screenHeight = self.view.frame.height
+        
+        label_Title1.adjustsFontSizeToFitWidth = true
+        label_Title2.adjustsFontSizeToFitWidth = true
+        
+        label_Title1.font = UIFont(name: "Beachday", size: screenHeight * title1FontScale)
+        label_Title2.font = UIFont(name: "Beachday", size: screenHeight * title2FontScale)
+    }
+    
+    private func animateLabels(withDelay delay: Int = 0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            self.label_Title1.morphingEffect = .fall
+            self.label_Title1.text = "Where is"
+            
+            self.label_Title2.morphingEffect = .scale
+            self.label_Title2.text = "ABC"
+        }
     }
 }
