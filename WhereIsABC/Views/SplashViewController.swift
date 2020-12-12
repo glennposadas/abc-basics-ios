80217ec654636af8bd768b909d0f425c5bbd4866
Glenn Posadas <hello@glennvon.com>
2020-12-13 02:38:46 +0800

    start update splash.

--- WhereIsABC/Views/SplashViewController.swift
+++ WhereIsABC/Views/SplashViewController.swift
@@ -13,7 +13,6 @@ class SplashViewController: BaseViewController {
     // MARK: - Properties
     
     @IBOutlet weak var label_Title1: LTMorphingLabel!
-    @IBOutlet weak var label_Title2: LTMorphingLabel!
     
     // MARK: - Overrides
     // MARK: Functions
@@ -21,35 +20,30 @@ class SplashViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupLabels()
+        setupViews()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        animateLabels(withDelay: 1)
+        animateViews(withDelay: 1)
     }
     
-    private func setupLabels() {
+    private func setupViews() {
         let title1FontScale: CGFloat = 0.2
-        let title2FontScale: CGFloat = 0.4
         
         let screenHeight = self.view.frame.height
         
         label_Title1.adjustsFontSizeToFitWidth = true
-        label_Title2.adjustsFontSizeToFitWidth = true
-        
         label_Title1.font = UIFont(name: "Beachday", size: screenHeight * title1FontScale)
-        label_Title2.font = UIFont(name: "Beachday", size: screenHeight * title2FontScale)
     }
     
-    private func animateLabels(withDelay delay: Int = 0) {
+    private func animateViews(withDelay delay: Int = 0) {
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
             self.label_Title1.morphingEffect = .fall
             self.label_Title1.text = "Where is"
             
-            self.label_Title2.morphingEffect = .scale
-            self.label_Title2.text = "ABC"
+            
         }
     }
 }
