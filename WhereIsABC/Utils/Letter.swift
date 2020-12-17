4d13c7204fb58233ec1e7651aff65ccbe27273cf
Glenn Posadas <hello@glennvon.com>
2020-12-17 21:42:07 +0800

    add Letter enum.

---
+++ WhereIsABC/Utils/Letter.swift
@@ -0,0 +1,12 @@
+//
+//  Alphabet.swift
+//  WhereIsABC
+//
+//  Created by Glenn Posadas on 12/17/20.
+//
+
+import Foundation
+
+enum Alphabet: CodingKey {
+    case A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+}
